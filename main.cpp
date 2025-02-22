1ec91eae32657f6307037af2f9e632281c2d9b3e
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2025-02-22 13:34:09 +0100

    add views-compilation step

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
-#include "webview/webview.h"
 #include <iostream>
+#include <string>
 
-int main() {
-    try {
+#include "views.h"
+#include "webview/webview.h"
+
+
+int main()
+{
+    // get homepage contents
+    const auto home_page = home;
+
+    try
+    {
+        int16_t count = 0;
+
         webview::webview w(false, nullptr);
-        w.set_title("Basic example");
-        w.set_size(480, 320, WEBVIEW_HINT_NONE);
-        w.set_html("Thanks for using webview!");
+        w.set_title("cauri");
+        w.set_size(1280, 720, WEBVIEW_HINT_NONE);
+        w.set_html(home_page);
+
+        w.bind("incr", [&](const std::string& req) -> std::string { return std::to_string(++count); });
         w.run();
-    } catch (const webview::exception &e) {
+    }
+    catch (const webview::exception& e)
+    {
         std::cerr << e.what() << '\n';
         return 1;
     }
